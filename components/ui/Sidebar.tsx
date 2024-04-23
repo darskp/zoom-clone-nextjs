fea617a100bb2ea1aab474ea8415c80fba3baad8
darskp <darshanpatgar100@gmail.com>
2024-04-23 23:08:03 +0530

    sidebar

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -4,6 +4,7 @@ import { sidebarlinks } from '@/constants'
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 
 const Sidebar = () => {
 const pathname=usePathname();
@@ -12,14 +13,21 @@ const pathname=usePathname();
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
       <div className='flex flex-col gap-6'>
         {sidebarlinks.map((link:any) => {
-            const isActive = pathname == link.route || pathname.startsWith(link.route);
+          const isActive = pathname == link.route || link.route !== '/' && pathname.startsWith(link.route);
+            
             return(
               <Link 
               key={link.label}
               href={link.route}
               className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{'bg-blue-1':isActive,})}
               >
-                {link.label}
+               <Image 
+               alt={link.label}
+               src={link.imgUrl}
+              width={24}
+              height={24}
+               />
+               <p className='text-lg  font-semibold max-lg:hidden'>{link.label}</p>
               </Link>
             )
         })}
